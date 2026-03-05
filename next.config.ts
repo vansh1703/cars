98293aa0854bc5aee6f28bc3fc09e7b2a0acf7ea
vansh1703 <vanshv1703@gmail.com>
2026-03-05 22:05:24 +0530

    f

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '*.supabase.co',
+        hostname: 'ufiaxnnftsrqbphvmqun.supabase.co',
+        port: '',
         pathname: '/storage/v1/object/public/**',
       },
     ],
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
